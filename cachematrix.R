558e1356d6f4efa113a6bcb5997a8b371f400c42
roman-se <roman@nemiga.net>
2015-06-19 14:47:39 +0300

    Created the functions from the wrapper code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates an instrument to cache the inverted matrixes
 
-## Write a short comment describing this function
-
+## Stores/retrieves the matrix as well as the inverses 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function(){
+        x
+    } 
+    setinverse <- function(inverse){
+        inv <<- inverse
+    } 
+    getinverse <- function() {
+        inv
+    }
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
